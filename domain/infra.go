77682733d8ff3f30e76df943906e689d132eb272
Jimmy <rivendinner@gmail.com>
2024-04-02 22:41:07 +0800

    backup

--- domain/infra.go
+++ domain/infra.go
@@ -2,6 +2,14 @@ package domain
 
 import "context"
 
+type DbRepository interface {
+	Ping(c context.Context) error
+	Get(c context.Context, edge Edge, queryMode bool) (edges []Edge, err error)
+	Create(c context.Context, edge Edge) error
+	Delete(c context.Context, edge Edge, queryMode bool) error
+	ClearAll(c context.Context) error
+}
+
 type GraphInfra interface {
 	Check(c context.Context, sbj Vertex, obj Vertex, searchCond SearchCond) (
 		found bool, err error)
